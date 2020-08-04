4d01ad9a25869de1557818ae6b27e96f50d0d14d
Tony Kim <hjkmines@gmail.com>
2020-08-03 22:53:41 -0600

    adds link to powers

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,7 @@
 const $ul = document.querySelector('ul')
 const $select = document.querySelector('select')
+const $powersUl = document.querySelector('.powers')
+
 
 // const searchParams = new URLSearchParams(window.location.search)
 // const param = searchParams.get()
@@ -18,6 +20,18 @@ fetch("http://localhost:3000/powers")
   .then(response => response.json())
   .then(response => {
     response.forEach((power) => {
+      const $li = document.createElement('li')
+      const $option = document.createElement('option')
       
+      $li.innerHTML = `<a href=./power.html?id=${power.id}>${power.name}</a>` 
+      $powersUl.append('$li')
+
+      $option.textContent = power.name
+      $option.name = "power_id" 
+      $option.value = power.id
+      
+      $select.append('$option')
+
+
     })
   })
