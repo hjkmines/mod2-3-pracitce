22257e7727c415b74961c1a2e866bdaec3a63f6f
Tony Kim <hjkmines@gmail.com>
2020-08-04 12:13:21 -0600

    adds delete controller

--- backend/app/models/hero.rb
+++ backend/app/models/hero.rb
@@ -1,8 +1,6 @@
 class Hero < ApplicationRecord
   belongs_to :power
 
-  validates :name, :presence => {:message => "Name cannot be blank"}
-  validates :super_name, :presence => {:message => "Super Name cannot be blank"}
-  validates :power_id, uniqueness: true, :presence => {:message => "Power ID cannot be blank"}
-
+  validates_presence_of :name, :super_name, :power_id
+  validates :super_name, uniqueness: true 
 end
